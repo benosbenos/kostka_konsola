183b16f92cc98cbf00b3f2dea497773438ffee55
benos <mikiwieczorek2006@gmail.com>
2024-12-16 10:19:03 +0100

    Dodaj pliki projektów.

---
+++ kostka/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        while (true)
+        {
+            int wynik = random.Next(1, 7);
+            Console.Clear();
+            Console.WriteLine("Wynik rzutu kostką: " + wynik);
+            Kostka(wynik);
+            Console.WriteLine("Wciśnij przycisk aby rzucić ponownie :)");
+            Console.ReadKey();
+            
+        }
+    }
+
+    static void Kostka(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("|       |");
+                Console.WriteLine("|   *   |");
+                Console.WriteLine("|       |");
+                Console.WriteLine("+-------+");
+                break;
+            case 2:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("| *     |");
+                Console.WriteLine("|       |");
+                Console.WriteLine("|     * |");
+                Console.WriteLine("+-------+");
+                break;
+            case 3:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("| *     |");
+                Console.WriteLine("|   *   |");
+                Console.WriteLine("|     * |");
+                Console.WriteLine("+-------+");
+                break;
+            case 4:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("|       |");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("+-------+");
+                break;
+            case 5:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("|   *   |");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("+-------+");
+                break;
+            case 6:
+                Console.WriteLine("+-------+");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("| *   * |");
+                Console.WriteLine("+-------+");
+                break;
+            default:
+                Console.WriteLine("Błąd: Niepoprawna liczba.");
+                break;
+        }
+    }
+}
+
